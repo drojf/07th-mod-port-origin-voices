fea15428abe5a28f90d44b59afb4bafe8967e051
Daniel Wong <1249449+drojf@users.noreply.github.com>
2023-08-25 23:23:07 +1000

    Extract replacements per file, and use names matching chapter file prefixes

--- extract_changed_voices.py
+++ extract_changed_voices.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import os
 import subprocess
 import json
@@ -34,26 +35,35 @@ def process_chapter(conf: ProcessSettings):
         lines = f.readlines()
 
     last_line = None
-    line_pairs = []
+    line_replacements_per_file = defaultdict(list)
+    current_file = None
 
     for line in lines:
+        if line.startswith('+++'):
+            current_file = line[6:]
+
         if last_line == None:
             last_line = line
             continue
 
         if last_line.startswith('-') and last_line.lstrip('-').strip().lower().startswith('modplayvoicels'):
             if line.startswith('+') and line.lstrip('+').strip().lower().startswith('modplayvoicels'):
-                line_pairs.append((last_line[1:].rstrip(), line[1:].rstrip()))
+                if current_file is None:
+                    raise Exception("Current file is unknown!")
+
+                line_replacements_per_file[current_file].append((last_line[1:].rstrip(), line[1:].rstrip()))
 
         last_line = line
 
-    return line_pairs
+    return line_replacements_per_file
 
+# Chapter name should match prefix of most scripts inside Update folder
 settings_list = [
-    ProcessSettings('onikakushi', 'C:/drojf/large_projects/umineko/HIGURASHI_REPOS/1 onikakushi', "bd2a98735890d10c365de93e9b16f71d59af662b", "72a2f10bdd0d9f4559c7345b783611cc649b4775"),
-    ProcessSettings('watanagashi', 'C:/drojf/large_projects/umineko/HIGURASHI_REPOS/2 watanagashi', "c88e8b898e8876515986fd0784f67d1b920bb6b2", "729fa6a2e0a99b1ad8b9dabf8e23c1c8a98b90e2"),
-    ProcessSettings('tatarigoroshi', 'C:/drojf/large_projects/umineko/HIGURASHI_REPOS/3 tatarigoroshi', "f4a894daa9f5c5353bbb65b916d6a897f1e615f5", "d9670d2b751c04164afccf7d269b05da38e50151"),
-    ProcessSettings('meakashi', 'C:/drojf/large_projects/umineko/HIGURASHI_REPOS/5 meakashi', "38516d0dae8b0786e6ab8777ec86c3ab6a7659ea", "ce5073b6bf137729ddc5e15ac67a96138bef9c0f")
+    ProcessSettings('onik', 'C:/drojf/large_projects/umineko/HIGURASHI_REPOS/1 onikakushi', "bd2a98735890d10c365de93e9b16f71d59af662b", "72a2f10bdd0d9f4559c7345b783611cc649b4775"),
+    ProcessSettings('wata', 'C:/drojf/large_projects/umineko/HIGURASHI_REPOS/2 watanagashi', "c88e8b898e8876515986fd0784f67d1b920bb6b2", "729fa6a2e0a99b1ad8b9dabf8e23c1c8a98b90e2"),
+    ProcessSettings('tata', 'C:/drojf/large_projects/umineko/HIGURASHI_REPOS/3 tatarigoroshi', "f4a894daa9f5c5353bbb65b916d6a897f1e615f5", "d9670d2b751c04164afccf7d269b05da38e50151"),
+    ProcessSettings('hima', 'C:/drojf/large_projects/umineko/HIGURASHI_REPOS/4 himatsubushi', "2a060413500634ef3e19ef21c148658caa0b6914", "94787c4280be455d21f9e0ca27fb9d5fadd636b1"),
+    ProcessSettings('_meak', 'C:/drojf/large_projects/umineko/HIGURASHI_REPOS/5 meakashi', "38516d0dae8b0786e6ab8777ec86c3ab6a7659ea", "ce5073b6bf137729ddc5e15ac67a96138bef9c0f")
 ]
 
 # Process each chapter, and collect as replacements per-chapter
@@ -61,7 +71,12 @@ chapter_to_replacements_list = {}
 for conf in settings_list:
     replacements = process_chapter(conf)
     chapter_to_replacements_list[conf.chapter_name] = replacements
-    print(f"Chapter {conf.chapter_name} had {len(replacements)} replacements at {conf.chapter_path} from {conf.before_voices_sha[0:6]}... to {conf.after_voices_sha[0:6]}...")
+
+    total_replacements = 0
+    for filename, file_replacements in replacements.items():
+        total_replacements += len(file_replacements)
+
+    print(f"Chapter {conf.chapter_name} had {len(replacements)} files with {total_replacements} replacements at {conf.chapter_path} from {conf.before_voices_sha[0:6]}... to {conf.after_voices_sha[0:6]}...")
 
 # Save all chapters replacements as json file
 with open('origin_replacements.json', 'w', encoding='utf-8') as f:
